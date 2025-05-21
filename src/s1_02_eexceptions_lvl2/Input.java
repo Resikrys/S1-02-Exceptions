3582e2fae4e141c2617fe19cee4f612b64d07c78
resikrys <resicris@gmail.com>
2025-05-21 10:52:45 +0200

    add InputMismatch byte, int, float, double exceptions

--- src/s1_02_eexceptions_lvl2/Input.java
+++ src/s1_02_eexceptions_lvl2/Input.java
@@ -9,14 +9,48 @@ public class Input {
     public static byte readByte(String message) {
         while(true) {
             try {
-                System.out.print(missatge + ": ");
+                System.out.print(message + ": ");
                 return scanner.nextByte();
             } catch (InputMismatchException e) {
-                System.out.println("Format error. Insert byte value.");
+                System.out.println("Format error. Insert a byte value.");
                 scanner.nextLine();
             }
         }
     }
 
-    public static int readInt(String message)
+    public static int readInt(String message) {
+        while(true) {
+            try {
+                System.out.print(message + ": ");
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Format error. Insert an int value.");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public static float readFloat(String message) {
+        while(true) {
+            try {
+                System.out.print(message + ": ");
+                return scanner.nextFloat();
+            } catch (InputMismatchException e) {
+                System.out.println("Format error. Insert a float value.");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public static double readDouble(String message) {
+        while(true) {
+            try {
+                System.out.print(message + ": ");
+                return scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Format error. Insert a double value.");
+                scanner.nextLine();
+            }
+        }
+    }
 }
